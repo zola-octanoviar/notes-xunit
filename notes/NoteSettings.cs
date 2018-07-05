ef81dc5697af39d3638129d28cf166802cc21c13
octanoviar <zola.octanoviar@gmail.com>
2018-07-05 11:56:14 +0700

    init

---
+++ notes/NoteSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace notes
+{
+    public class NoteSettings
+    {
+        public string Name { get; set; } = "notes";
+        public string Description { get; set; } = "A short description of the note";
+        public string Owner { get; set; } = "The Owner";
+        public int PostsPerPage { get; set; } = 2;
+        public int CommentsCloseAfterDays { get; set; } = 10;
+    }
+}
---
+++ notes_test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace notes_test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
